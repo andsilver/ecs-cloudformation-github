9242662f7bcfff46f0bfb4d25b622d94af11ba31
EL <elicloudh@gmail.com>
2019-12-25 06:03:37 +0200

    update

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', (_req, res) => {
 	return res.json({ success: 'hello' });
 });
 
+app.get('/abc', (_req, res) => {
+	return res.json({ success: 'hello' });
+});
+
 app.listen(80, () => console.log('listening on port 3000')); // new
